14f91e5ee58d7be330b9ff13dc5a831dfe636c09
Marin Cholakov <marinvch@gmail.com>
2020-06-06 09:39:37 +0300

    search bar is only loginng input with length greater than 1

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,33 +1,46 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-main-nav',
-  templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  selector: "app-main-nav",
+  templateUrl: "./main-nav.component.html",
+  styleUrls: ["./main-nav.component.css"],
 })
 export class MainNavComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
+  public toggle(): void {
+    const toolbar = (document.querySelectorAll(
+      "mat-toolbar"
+    )[1] as unknown) as HTMLElement;
+
+    if (toolbar.style.display === "none") {
+      toolbar.style.display = "block";
+    } else {
+      toolbar.style.display = "none";
+    }
   }
 
-  public search(): void {
-    const toolbar = document.querySelectorAll('mat-toolbar')[1] as unknown as HTMLElement;
+  public search() {
+    const input = document.querySelector("input");
+    input.addEventListener("input", searchData);
 
-    if (toolbar.style.display === 'none') {
-      toolbar.style.display = 'block';
-    } else {
-      toolbar.style.display = 'none';
-
+    function searchData() {
+      let data = input.value;
+      if (data.length >= 2) {
+        setTimeout(function () {
+          return console.log(data);
+        }, 2000);
+      }
     }
   }
 }
