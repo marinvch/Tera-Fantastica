84fd9dc4303a10fb73188904542115225ae047c3
Marin Cholakov <marinvch@gmail.com>
2020-06-06 09:55:26 +0300

    add Json for the book component

--- src/app/books.ts
+++ src/app/books.ts
@@ -1,12 +1,11 @@
 export interface IBooks {
   url: string;
-  text: {
-    name: string;
-    author: string;
-    Year: number;
-    Format: string;
-    Pages: number;
-    id: number;
-    link: string;
-  };
+  text: Object;
+  name: string;
+  author: string;
+  Year: number;
+  Format: string;
+  Pages: number;
+  id: number;
+  link: string;
 }
