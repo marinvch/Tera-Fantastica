b87bd66369f3dc74acbe2fdc042ba15e1e007a4d
Marin Cholakov <marinvch@gmail.com>
2021-12-28 16:44:43 +0200

    added navigation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Newspaper from "./pages/Newspaper";
+import Books from "./pages/Books";
+import Magazines from "./pages/Magazines";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="newspaper" exact element={<Newspaper />} />
+        <Route path="books" element={<Books />} />
+        <Route path ="magazines" element={<Magazines />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
